654a477df7f19de29a4c32b4fc71393efb68784c
Roxana Maghiar <roxana4x4@yahoo.com>
2021-05-17 06:03:14 +0300

    String or String numbers Check

--- Ex1.py
+++ Ex1.py
@@ -1,1 +1,6 @@
-print('buna seara')
+name = input("Write your name:")
+text = input("Write a text (characters or numbers):")
+if text.isdigit():
+    print(f" The text made out of numbers was found by {name}.")
+else:
+    print(f" The text made out of characters was found by {name}.")
